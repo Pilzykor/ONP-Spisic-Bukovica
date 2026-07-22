ccfb73f99ac7bfe6db6e0aa34af499255ab0b530
Kreso <117861902+Pilzykor@users.noreply.github.com>
2026-07-22 18:37:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,9 @@
 "use strict";
 
-/**
- * Supabase konfiguracija.
- *
- * Ovdje smije biti samo:
- * - Project URL
- * - publishable key ili legacy anon key
- *
- * Nikada nemoj ovdje staviti:
- * - service_role key
- * - secret key
- * - lozinku baze
- */
-
 window.APP_CONFIG = Object.freeze({
   supabaseUrl: "https://flkqaucgcfldmlqlqbsz.supabase.co",
-  supabaseKey: "sb_publishable_6YgNS4XTEnN49G_1PjBZ1g_TOE4F1FA"
+  supabaseKey: "sb_publishable_6YgNS4XTEnN49G_1PjBZ1g_TOE4F1FA",
+
+  authEmailDomain: "@proton.me",
+  defaultLocation: "Špišić Bukovica"
 });
